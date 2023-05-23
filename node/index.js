015014ef7e81bf054adabc0b062d0a00fd278ad8
Gleides Vinente <gleidevelop@gmail.com>
2023-05-22 20:58:29 -0400

    Finalização dos exercícios I ao V na pasta node do repo

--- node/index.js
+++ node/index.js
@@ -1,19 +1,27 @@
 const http = require("http");
 const fs = require("fs");
 require("dotenv").config();
+const utils = require("./utils");
 
-PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3333;
 const folder = process.argv[2];
 
 const server = http.createServer((req, res) => {
-  fs.readdir(folder, (err, files) => {
-    if (err) console.log(err);
-    else {
-      res.writeHead(200, { "Content-type": "text/html; charset=utf-8" });
-      files.forEach((f) => res.write(`${f} <br>`));
-      res.end("Listando os arquivo de pasta");
-    }
-  });
+  if (req.url === "/") {
+    fs.readdir(folder, (err, files) => {
+      if (err) console.log(err);
+      else {
+        res.writeHead(200, { "Content-type": "text/html; charset=utf-8" });
+        files.forEach((f) => res.write(`${utils.createLink(f)}`));
+        res.end("Listando os arquivos da pasta");
+      }
+    });
+  } else {
+    fs.readFile(`${folder}/${req.url}`, (err, content) => {
+      res.write(utils.createVoltar());
+      res.end(content);
+    });
+  }
 });
 
 server.listen(PORT, () => {
