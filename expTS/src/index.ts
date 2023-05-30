21ccb4f797462c979476f5bd4f5a2f9f7ec0c003
Gleides Vinente <gleidevelop@gmail.com>
2023-05-30 19:23:58 -0400

    Exercicio 05

--- expTS/src/index.ts
+++ expTS/src/index.ts
@@ -1,14 +1,22 @@
-import express from "express";
-import validateEnv from "./utils/validateEnv";
-import dotenv from "dotenv";
+import express from 'express';
+import validateEnv from './utils/validateEnv';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
 dotenv.config();
 validateEnv();
 
 const PORT = process.env.PORT ?? 3333;
 const app = express();
+app.use(morgan('combined'));
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Web Academy");
+app.use((req, res, next) => {
+  console.log('oi');
+  next();
+});
+
+app.get('/welcome', (req, res) => {
+  res.send('Welcome to Web Academy');
 });
 
 app.listen(PORT, () => {
